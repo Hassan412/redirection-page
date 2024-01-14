b35dd0dbeabbf5805368a43fd15c3d8297bced45
Hassan <121817992+Hassan412@users.noreply.github.com>
2024-01-14 22:23:54 +0500

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,7 +5,7 @@ import { stripe } from "@/lib/stripe";
 import axios from "axios";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_MAIN_URL,
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization, x-api-key",
 };
